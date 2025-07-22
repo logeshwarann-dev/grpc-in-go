e60b09c344baa3d5731c17230f04276638d8c421
Logeshwaran N <logeshwaran082@gmail.com>
2025-07-22 20:16:09 +0530

    adduser handler is working

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,20 +1,37 @@
-package server
+package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"net"
 
+	"github.com/logeshwarann-dev/grpc-in-go/internal/api/handlers"
+	"github.com/logeshwarann-dev/grpc-in-go/internal/repository/mongodb"
 	pb "github.com/logeshwarann-dev/grpc-in-go/proto/rpcgen"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+func init() {
+
+	if err := mongodb.ConnectToMongo(); err != nil {
+		log.Fatalf("Failed connecting to DB: %v", err.Error())
+	}
+	log.Println("DB Connection successful!")
+}
 
 type Server struct {
 	pb.UnimplementedUserManagementServer
 }
 
 func (rpcServer *Server) AddUser(ctx context.Context, newUser *pb.NewUser) (*pb.UserResponse, error) {
-
-	return nil, status.Errorf(codes.Unimplemented, "method AddUser not implemented")
+	response, err := handlers.CreateUser(newUser)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("%v", err))
+	}
+	return response, nil
 }
 
 func (rpcServer *Server) DeleteUser(context.Context, *pb.UserId) (*pb.ResponseMessage, error) {
@@ -27,4 +44,23 @@ func (rpcServer *Server) GetUser(context.Context, *pb.UserId) (*pb.UserResponse,
 
 func (rpcServer *Server) UpdateUser(context.Context, *pb.ModifiedUser) (*pb.UserResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateUser not implemented")
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("unable to listen on port: ", err.Error())
+	}
+
+	log.Println("TCP Port 8080 is assigned.")
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterUserManagementServer(grpcServer, &Server{})
+
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalln("unable to start grpc server: ", err)
+	}
+
+	log.Println("gRPC Server has started!")
 }
--- internal/api/handlers/add_user.go
+++ internal/api/handlers/add_user.go
@@ -1,11 +1,28 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/logeshwarann-dev/grpc-in-go/internal/models"
 	"github.com/logeshwarann-dev/grpc-in-go/internal/repository/mongodb"
+	pb "github.com/logeshwarann-dev/grpc-in-go/proto/rpcgen"
 )
 
-func CreateUser(newUser models.User) error {
-	mongodb.InsertRecordInDB(newUser)
-	return nil
+func CreateUser(user *pb.NewUser) (*pb.UserResponse, error) {
+	newUser := models.User{
+		FistName: user.FirstName,
+		LastName: user.LastName,
+		Age:      int(user.Age),
+		Email:    user.Email,
+		PhNo:     user.PhNo,
+	}
+	userId, err := mongodb.InsertRecordInDB(newUser)
+	if err != nil {
+		return nil, fmt.Errorf("unable to Add new user: %v", err.Error())
+	}
+	userRes := pb.UserResponse{
+		Id:   userId,
+		User: user,
+	}
+	return &userRes, nil
 }
